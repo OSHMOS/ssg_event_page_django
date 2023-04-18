9aa2eee8e5a463dcb1f5e939dbbbb1d2bc5f6132
oshmos <ojames9707@gmail.com>
2023-04-18 18:29:11 +0900

    이벤트 모델 생성 및 폼 출력 완, 어드민에서 이벤트 생성 및 사진 파일 확인 테스트 완

--- event/models.py
+++ event/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Event(models.Model):
+    dept = models.CharField(max_length=20)
+    num = models.CharField(max_length=10)
+    name = models.CharField(max_length=50)
+    photo = models.ImageField(upload_to='img/')
+
+    def __str__(self):
+        return f'{self.dept}, {self.num}, {self.name}'
--- event/views.py
+++ event/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
-
+from event.forms import EventForm
 # Create your views here.
 
 
 def upload(request):
+    form = EventForm()
     if request.method == 'POST':
         pass
-    return render(request, 'event/form.html')
+    return render(request, 'event/form.html', {'form': form})
