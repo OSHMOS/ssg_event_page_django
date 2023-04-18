f0a12f884b655c0be5e1043b7ea696fa455915c6
oshmos <ojames9707@gmail.com>
2023-04-18 22:53:30 +0900

    사용자가 이벤트 모델 생성 가능 및 테스팅 완

--- event/models.py
+++ event/models.py
@@ -4,10 +4,10 @@ from django.db import models
 
 
 class Event(models.Model):
-    dept = models.CharField(max_length=20)
-    num = models.CharField(max_length=10)
-    name = models.CharField(max_length=50)
-    photo = models.ImageField(upload_to='img/')
+    dept = models.CharField(max_length=20, verbose_name='학과')
+    num = models.CharField(max_length=10, verbose_name='학번')
+    name = models.CharField(max_length=50, verbose_name='이름')
+    photo = models.ImageField(upload_to='img/', verbose_name='인증 사진')
 
     def __str__(self):
         return f'{self.dept}, {self.num}, {self.name}'
--- event/views.py
+++ event/views.py
@@ -3,8 +3,14 @@ from event.forms import EventForm
 # Create your views here.
 
 
-def upload(request):
+def certify(request):
     form = EventForm()
     if request.method == 'POST':
-        pass
+        form = EventForm(request.POST, request.FILES)
+        if form.is_valid():
+            event = form.save()
+            event.save()
+            return render(request, 'event/thx.html')
+        else:
+            print(form.errors.as_data())
     return render(request, 'event/form.html', {'form': form})
