6436de2026a224f76f31e5bf3b2e64f0f0cd52f1
oshmos <ojames9707@gmail.com>
2023-05-03 03:08:30 +0000

    deploy soongsilgios_event_page through pythonanywhere:paas - 05.03

--- config/settings.py
+++ config/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)1e@+$!ton76)+46l@hucyawh5xjho68)l%_c-yfzll8y%r=3*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*',]
 
